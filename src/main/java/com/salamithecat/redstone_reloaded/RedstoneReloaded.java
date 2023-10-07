e89649f394895856eb3e6dade6f5822a2f584053
PeeblyWeeb <PebblyKat@gmail.com>
2023-10-07 12:56:53 -0400

    Initial commit

---
+++ src/main/java/com/salamithecat/redstone_reloaded/RedstoneReloaded.java
@@ -0,0 +1,11 @@
+package com.salamithecat.redstone_reloaded;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RedstoneReloaded implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/salamithecat/redstone_reloaded/client/RedstoneReloadedClient.java
@@ -0,0 +1,10 @@
+package com.salamithecat.redstone_reloaded.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedstoneReloadedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
